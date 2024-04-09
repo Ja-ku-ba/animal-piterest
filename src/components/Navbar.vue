fcb7d8dc1deba7fbec0e5474366d8072906bf62e
Ubuntu <wulfrykpierozek@gmail.com>
2024-04-09 20:44:46 +0200

    init + nabar init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,73 @@
+<script setup>
+import Searchbar from "./elements/Searchbar.vue"
+</script>
+
+<template>
+  <nav class="navbar-container">
+    <ul class="center">
+      <li class="center logo-wrapper">
+        <img src="../assets/dog_icon.svg" class="logo" alt="Strona główna" />
+      </li>
+      <li class="center">
+        <a class="main-page-link nav-box" href="/"
+          ><span class="main-page-text">Strona główna</span></a
+        >
+      </li>
+      <li class="center">
+        <a class="create-page-link nav-box" href="/"
+          ><span class="main-page-text">Utwórz</span></a
+        >
+      </li>
+      <li>
+        <Searchbar />
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.navbar-container {
+  position: sticky;
+  height: 80px;
+  width: 100%;
+  top: 0;
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+}
+
+.center {
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  padding: 0;
+  align-items: center;
+}
+
+.logo-wrapper {
+  border-radius: 50%;
+  width: 24px;
+  height: 24px;
+  border: 24px solid transparent;
+  background-clip: content-box;
+  transition: transform 0.5s ease;
+}
+
+.logo-wrapper:hover {
+  border: 24px solid rgba(0, 0, 0, 0.1);
+  background-color: rgba(0, 0, 0, 0.1);
+}
+
+.nav-box {
+  border-radius: 24px;
+  padding: 0 24px;
+}
+
+.main-page-link {
+  background-color: rgba(17, 17, 17, 17);
+  color: white;
+  height: 48px;
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/components/elements/Searchbar.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+
+<template>
+  <form class="search-container center nav-box">
+    <img src="../../assets/search.svg" class="logo search-icon" alt="search" />
+
+    <input class="nav-input" type="text" placeholder="Szukaj" />
+  </form>
+</template>
+
+<style scoped>
+.search-container {
+  height: 48px;
+  width: 100%;
+  background-color: rgb(241, 241, 241);
+  border-radius: 24px;
+}
+
+.search-icon {
+  margin-left: -8px;
+}
+
+.nav-input {
+  width: 90%;
+  margin-right: auto;
+  border: 0;
+  height: 24px;
+  background-color: inherit;
+  outline: none;
+  font-size: 16px;
+}
+</style>
