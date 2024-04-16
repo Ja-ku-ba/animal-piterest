2e2daf58621fbf57e66e31c1c1e31c8d8b07bed0
Ubuntu <wulfrykpierozek@gmail.com>
2024-04-16 12:28:14 +0200

    pinterest reactive feed recreated

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from "./components/Navbar.vue"
 import TagsCarousel from "./components/TagsCarousel.vue"
+import Feed from "./components/Feed.vue"
 </script>
 
 <template>
   <Navbar />
   <TagsCarousel />
+  <Feed />
 </template>
 
 <style scoped></style>
---
+++ src/components/Feed.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="pin_container">
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/dog.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/bear.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/peungin.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/fox.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/hamster.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/cat.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/bear.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/peungin.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/hedgehog.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/deer.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/bear.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/peungin.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/dog.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/bear.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/peungin.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/bear.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/peungin.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/bear.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/peungin.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/dog.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/bear.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/peungin.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/bear.jpg" />
+    </div>
+    <div class="card">
+      <img @load="setBorder" class="img" src="../assets/images/peungin.jpg" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+const height = ref(null)
+function setBorder(e) {
+  height.value = e.target.height
+  const rowEnd = Math.ceil(height.value / 10) + 2
+  console.log(rowEnd)
+  e.target.parentElement.style = `grid-row-end: span ${rowEnd}`
+}
+</script>
+
+<style scoped>
+.pin_container {
+  width: 100%;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 250px);
+  grid-auto-rows: var(--row_increment);
+  justify-content: center;
+}
+
+.card {
+  padding: 0;
+  margin: 15px 10px;
+  border-radius: var(--card_border_radius);
+  background-color: wheat;
+}
+
+.img {
+  border-radius: 16px;
+  width: var(--card-with);
+}
+</style>
